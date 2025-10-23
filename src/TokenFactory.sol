94c0b496bb1b6db55d0390a89cf49539a22151b9
JetJadeja <jetjadeja@gmail.com>
2025-10-23 12:18:17 -0700

    add seicho elements to token factory

--- src/TokenFactory.sol
+++ src/TokenFactory.sol
@@ -4,12 +4,34 @@ pragma solidity ^0.8.24;
 import { ITokenFactory } from "src/interfaces/ITokenFactory.sol";
 import { DERC20 } from "src/DERC20.sol";
 import { ImmutableAirlock } from "src/base/ImmutableAirlock.sol";
+import { IRegistry, IStrategyMaker, IVault } from "src/interfaces/ISeicho.sol";
+import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/token/ERC20/utils/SafeERC20.sol";
 
 /// @custom:security-contact security@whetstone.cc
 contract TokenFactory is ITokenFactory, ImmutableAirlock {
+    using SafeERC20 for IERC20;
+
+    IStrategyMaker public immutable strategyMaker;
+    IRegistry public immutable tacticRegistry;
+    IRegistry public immutable vaultRegistry;
+
+    /**
+     * @param airlock_ Address of the Airlock contract
+     * @param strategyMaker_ Address of Seicho StrategyMaker (address(0) if not using distributions)
+     * @param tacticRegistry_ Address of Seicho TacticRegistry (address(0) if not using distributions)
+     * @param vaultRegistry_ Address of Seicho VaultRegistry (address(0) if not using distributions)
+     */
     constructor(
-        address airlock_
-    ) ImmutableAirlock(airlock_) { }
+        address airlock_,
+        address strategyMaker_,
+        address tacticRegistry_,
+        address vaultRegistry_
+    ) ImmutableAirlock(airlock_) {
+        strategyMaker = IStrategyMaker(strategyMaker_);
+        tacticRegistry = IRegistry(tacticRegistry_);
+        vaultRegistry = IRegistry(vaultRegistry_);
+    }
 
     /**
      * @notice Creates a new DERC20 token
